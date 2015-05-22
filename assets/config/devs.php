6867dc337b152046e0b34eddca4a5e08a1276428
Nicolas Widart <n.widart@gmail.com>
2015-05-22 20:02:17 +0200

    Adding Nicolas Widart.

--- assets/config/devs.php
+++ assets/config/devs.php
@@ -1,13 +1,13 @@
 <?php
 
 return array (
- 
+
   array (
     'name' => 'Adam Wathan',
     'desc' => 'Fullstackradio host',
     'twitter' => 'adamwathan',
   ),
- 
+
   array (
     'name' => 'Alex Bilbie',
     'desc' => 'PHP League member',
@@ -19,13 +19,13 @@ return array (
     'desc' => 'Yii2',
     'twitter' => 'sam_dark',
   ),
- 
+
   array (
     'name' => 'Andi Gutmans',
     'desc' => 'PHP Co-Architect',
     'twitter' => 'andigutmans',
   ),
- 
+
   array (
     'name' => 'Andrés Gutiérrez',
     'desc' => 'Phalcon creator',
@@ -37,7 +37,7 @@ return array (
     'desc' => 'Slim framework developer',
     'twitter' => 'silentworks',
   ),
- 
+
   array (
     'name' => 'Angela Byron',
     'desc' => 'Drupal Core developer',
@@ -47,164 +47,164 @@ return array (
     'name' => 'Anna Filina',
     'desc' => 'ConFoo Organizer',
     'twitter' => 'afilina',
-   ), 
- 
+   ),
+
   array (
     'name' => 'Anthony Ferrara',
     'desc' => 'PHP security advocate',
     'twitter' => 'ircmaxell',
   ),
- 
+
   array (
     'name' => 'Beau D. Simensen',
     'desc' => 'Sculpin creator, PHP-FIG member',
     'twitter' => 'beausimensen',
   ),
- 
+
   array (
     'name' => 'Ben Edmunds',
     'desc' => 'PHP Town Hall host',
     'twitter' => 'benedmunds',
   ),
- 
+
   array (
     'name' => 'Benjamin Eberlei',
     'desc' => 'Doctrine Lead',
     'twitter' => 'beberlei',
   ),
- 
+
   array (
     'name' => 'Bruno Skvorc',
     'desc' => 'SitePoint PHP Editor',
     'twitter' => 'bitfalls',
   ),
- 
+
   array (
     'name' => 'Cal Evans',
     'desc' => 'Developer Experience Architect',
     'twitter' => 'calevans',
   ),
- 
+
   array (
     'name' => 'Chris Fidao',
     'desc' => 'Laravel contributor',
     'twitter' => 'fideloper',
   ),
- 
+
   array (
     'name' => 'Chris Hartjes',
     'desc' => 'Little Book of Hack',
     'twitter' => 'grmpyprogrammer',
   ),
- 
+
   array (
     'name' => 'Christopher Pitt',
     'desc' => 'SilverStripe developer',
     'twitter' => 'assertchris',
   ),
- 
+
   array (
     'name' => 'Dave Marshall',
     'desc' => 'That Podcast host',
     'twitter' => 'davedevelopment',
   ),
- 
+
   array (
     'name' => 'Davey Shafik',
     'desc' => 'Community Engineer at Engine Yard',
     'twitter' => 'dshafik',
   ),
- 
+
   array (
     'name' => 'David Zuelke',
     'desc' => 'Heroku',
     'twitter' => 'dzuelke',
   ),
- 
+
   array (
     'name' => 'Derick Rethans',
     'desc' => 'XDebug author',
     'twitter' => 'derickr',
   ),
- 
+
   array (
     'name' => 'Dracony (Roman Tsiupa)',
     'desc' => 'Author of the PHPixie framework',
     'twitter' => 'dracony_gimp',
   ),
- 
+
   array (
     'name' => 'Dries Buytaert',
     'desc' => 'Drupal project lead',
     'twitter' => 'Dries',
   ),
- 
+
   array (
     'name' => 'Ed Finkler',
     'desc' => '/dev/hell podcast host',
     'twitter' => 'funkatron',
   ),
- 
+
   array (
     'name' => 'Elizabeth Smith',
     'desc' => 'PHP Core developer',
     'twitter' => 'auroraeosrose',
   ),
- 
+
   array (
     'name' => 'Eric Barnes',
     'desc' => 'Laravel News',
     'twitter' => 'ericlbarnes',
   ),
- 
+
   array (
     'name' => 'Erika Heidi Reinaldo',
     'desc' => 'PHP developer at Digital Ocean',
     'twitter' => 'erikaheidi',
   ),
- 
+
   array (
     'name' => 'Fabian Blechschmidt',
     'desc' => 'PHPucEU',
     'twitter' => 'fabian_ikono',
   ),
- 
+
   array (
     'name' => 'Fabien Potencier',
     'desc' => 'Symfony project lead',
     'twitter' => 'fabpot',
   ),
- 
+
   array (
     'name' => 'Fabrizio Branca',
     'desc' => 'Lead developer at AOE',
     'twitter' => 'fbrnc',
   ),
- 
+
   array (
     'name' => 'Frank De Jonge',
     'desc' => 'PHP League member',
     'twitter' => 'frankdejonge',
   ),
- 
+
   array (
     'name' => 'Franz Liedke',
     'desc' => 'FluxBB developer',
     'twitter' => 'franzliedke',
   ),
- 
+
   array (
     'name' => 'Graham Campbell',
     'desc' => 'Laravel contributor',
     'twitter' => 'grahamjcampbell',
   ),
- 
+
   array (
     'name' => 'Jack Skinner',
     'desc' => 'SydPHP organizer',
     'twitter' => 'developerjack',
   ),
- 
+
  array (
     'name' => 'James Titcumb',
     'desc' => 'Runs PHP South Coast',
@@ -216,31 +216,31 @@ return array (
     'desc' => 'Laracasts creator',
     'twitter' => 'jeffrey_way',
   ),
- 
+
   array (
     'name' => 'Jeremy Lindblom',
     'desc' => 'Organizer of PNWPHP and SeaPHP',
     'twitter' => 'jeremeamia',
   ),
- 
+
   array (
     'name' => 'Jonathan Reinink',
     'desc' => 'PHP League member',
     'twitter' => 'reinink',
   ),
- 
+
   array (
     'name' => 'Jonathan Wage',
     'desc' => 'Opensky',
     'twitter' => 'jwage',
   ),
- 
+
   array (
     'name' => 'Jordi Boggiano',
     'desc' => 'Author of Composer',
     'twitter' => 'seldaek',
   ),
- 
+
   array (
     'name' => 'Jose Diaz-Gonzalez',
     'desc' => 'CakePHP Core developer',
@@ -258,25 +258,25 @@ return array (
     'desc' => 'Slim framework creator',
     'twitter' => 'codeguy',
   ),
- 
+
   array (
     'name' => 'Juan Treminio',
     'desc' => 'PuPHPet creator',
     'twitter' => 'juantreminio',
   ),
-    
+
   array (
     'name' => 'Kayla Daniels',
     'desc' => 'PHP-FIG member',
     'twitter' => 'kayladnls',
   ),
- 
+
   array (
     'name' => 'Konstantin Kudryashov',
     'desc' => 'Behat developer',
     'twitter' => 'everzet',
   ),
- 
+
   array (
     'name' => 'Kris Wallsmith',
     'desc' => 'Assetic',
@@ -288,19 +288,19 @@ return array (
     'desc' => 'Drupal',
     'twitter' => 'crell',
   ),
- 
+
   array (
     'name' => 'Lorna Jane Mitchell',
     'desc' => 'Developer at Siftware',
     'twitter' => 'lornajane',
   ),
- 
+
   array (
     'name' => 'Lukas Kahwe Smith',
     'desc' => 'Liip developer',
     'twitter' => 'lsmith',
   ),
-  
+
   array (
     'name' => 'Marco Pivetta',
     'desc' => 'Doctrine developer',
@@ -311,145 +311,151 @@ return array (
     'desc' => 'CakePHP lead developer',
     'twitter' => 'mark_story',
   ),
- 
+
   array (
     'name' => 'Matt Stauffer',
     'desc' => 'Blogger',
     'twitter' => 'stauffermatt',
   ),
- 
+
   array (
     'name' => 'Matthew Weier O’Phinney',
     'desc' => 'Zend Framework developer',
     'twitter' => 'mwop',
   ),
- 
+
   array (
     'name' => 'Michael Dowling',
     'desc' => 'Guzzle creator',
     'twitter' => 'mtdowling',
   ),
- 
+
   array (
     'name' => 'Michelle Sanver',
     'desc' => 'PHPwomen',
     'twitter' => 'michellesanver',
   ),
-    
+
   array (
     'name' => 'Mike van Riel',
     'desc' => 'phpDocumentor',
     'twitter' => 'mvriel',
   ),
-    
+
   array (
     'name' => 'Mior Muhammad Zaki',
     'desc' => 'Orchestra Platform',
     'twitter' => 'crynobone',
   ),
-    
+
   array (
     'name' => 'Nate Abele',
     'desc' => 'Lithium author',
     'twitter' => 'nateabele',
   ),
- 
+
+  array (
+      'name' => 'Nicolas Widart',
+      'desc' => 'AsgardCms creator',
+      'twitter' => 'nicolaswidart',
+  ),
+
   array (
     'name' => 'Nikita Popov',
     'desc' => 'PHP Core contributor',
     'twitter' => 'nikita_ppv',
   ),
- 
+
   array (
     'name' => 'Pádraic Brady',
     'desc' => 'PHP-FIG member',
     'twitter' => 'padraicb',
   ),
- 
+
   array (
     'name' => 'Paul M. Jones',
     'desc' => 'AuraPHP developer',
     'twitter' => 'pmjones',
   ),
- 
+
   array (
     'name' => 'Phil Sturgeon',
     'desc' => 'Master of the PHPUniverse',
     'twitter' => 'philsturgeon',
   ),
- 
+
   array (
     'name' => 'Phillip Jackson',
     'desc' => 'Magetalk host',
     'twitter' => 'philwinkle',
   ),
- 
+
   array (
     'name' => 'Rafael Dohms',
     'desc' => 'Amsterdam PHP community leader',
     'twitter' => 'rdohms',
   ),
- 
+
   array (
     'name' => 'Rasmus Lerdorf',
     'desc' => 'PHP Creator',
     'twitter' => 'rasmus',
   ),
- 
+
   array (
     'name' => 'Rob Allen',
     'desc' => 'Zend and Slim contributor',
     'twitter' => 'akrabat',
   ),
- 
+
   array (
     'name' => 'Ross Tuck',
     'desc' => 'DomCode co-founder',
     'twitter' => 'rosstuck',
   ),
- 
+
   array (
     'name' => 'Sammy Kaye Powers',
     'desc' => 'PHP Roundtable host',
     'twitter' => 'sammyk',
   ),
- 
+
   array (
     'name' => 'Sara Golemon',
     'desc' => 'HHVM',
     'twitter' => 'saramg',
   ),
- 
+
   array (
     'name' => 'Sebastian Bergmann',
     'desc' => 'Author of PHPUnit',
     'twitter' => 's_bergmann',
   ),
- 
+
   array (
     'name' => 'Shameer',
     'desc' => 'PHPToday',
     'twitter' => 'shameerc',
   ),
- 
+
   array (
     'name' => 'Shawn McCool',
     'desc' => 'Laravel contributor',
     'twitter' => 'ShawnMcCool',
   ),
- 
+
   array (
     'name' => 'Taylor Otwell',
     'desc' => 'Creator of Laravel',
     'twitter' => 'taylorotwell',
   ),
- 
+
   array (
     'name' => 'Tim Bezhashvyly',
     'desc' => 'Magekarma',
     'twitter' => 'tim_bezhashvyly',
   ),
- 
+
   array (
     'name' => 'William Durand',
     'desc' => 'Propel developer',
@@ -461,13 +467,13 @@ return array (
     'desc' => 'Author of YAF',
     'twitter' => 'laruence',
   ),
- 
+
   array (
     'name' => 'Yitzchok Willroth',
     'desc' => 'Code Rabbi',
     'twitter' => 'coderabbi',
   ),
- 
+
   array (
     'name' => 'Zeev Suraski',
     'desc' => 'Zend co-founder',
